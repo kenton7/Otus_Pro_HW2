1dca9dadd5fb74bcd9c58fda905507e28e6c724c
Ilya Kuznetsov <kuznecov95-95@yandex.ru>
2024-02-12 18:31:06 +0300

    Initial Commit

---
+++ Otus_Pro_2/Otus_Pro_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Otus_Pro_2App.swift
+//  Otus_Pro_2
+//
+//  Created by Илья Кузнецов on 12.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct Otus_Pro_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
