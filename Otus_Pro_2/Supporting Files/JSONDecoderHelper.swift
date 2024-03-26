3bad7a6131066d8d6990a5ebeb8f73223b23853d
Ilya Kuznetsov <kuznecov95-95@yandex.ru>
2024-03-26 14:53:54 +0300

    Added JSON Decoder Helper

---
+++ Otus_Pro_2/Supporting Files/JSONDecoderHelper.swift
@@ -0,0 +1,15 @@
+//
+//  JSONDecoder.swift
+//  Otus_Pro_2
+//
+//  Created by Илья Кузнецов on 26.03.2024.
+//
+
+import Foundation
+
+final class JSONDecoder {
+    static func getDecoder() -> JSONDecoder {
+        let decoder = JSONDecoder()
+        return decoder
+    }
+}
